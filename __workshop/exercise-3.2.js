80951e5d68c7d210cf40eba145b30142a57ff074
Jeffrey <jeffreyplacido@hotmail.com>
2020-07-10 17:30:26 -0400

    E3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,24 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
-
 const opencage = require('opencage-api-client');
 require('dotenv').config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
-  };
+    q: address,
+    key: process.env.OPENCAGE_API_KEY,
+    //or key: 'b4067c8a1a87455d8ca0f9569d9aed05',
+  }
 
-  // return something...
+  return opencage
+    .geocode(requestObj)
+    .then(response => response.results[0].geometry)
+    .catch(error => {
+      console.log('error', error.message);
+    });
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  '8618 Rue Drolet, Montréal, QC H2P 2H9'
 ).then((response) => console.log(response));
+
