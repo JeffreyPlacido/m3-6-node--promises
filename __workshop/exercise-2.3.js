af2911281e3ddc58cccc35571a7a56cd880e6639
Jeffrey <jeffreyplacido@hotmail.com>
2020-07-10 15:57:37 -0400

    E2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -5,3 +5,16 @@
 const request = require('request-promise');
 
 // get the code you wrote in 2.2 and paste it here...
+
+const greeting = (langCode) => {
+  return request("https://journeyedu.herokuapp.com/hello/" + langCode) // 1
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+    return parsedResponse.data.text;
+    })
+    .catch((err) => {
+        return err.error ? JSON.parse(err.error) : err;
+    });
+};
+
+greeting('PORTUGAL').then((result) => console.log(result));
